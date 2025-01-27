64421e8e30378e9c7c937d6b4e65cfb6115b9e79
Suyog0 <dahalsunischit@gmail.com>
2025-01-27 20:07:08 +0545

    updated model to contain all nepali alphabets

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -2,7 +2,6 @@ import React, { forwardRef } from 'react';
 import Video from './Video';
 import styles from './VideoContainer.module.css';
 const VideoContainer = forwardRef(({ refs }, ref) => {
-
     return (
         <div className={styles.container}>
             <Video ref={refs[0]} muted={true} />
